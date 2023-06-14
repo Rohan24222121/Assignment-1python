aedf183543cff660b6e67070a92da6b5fe76e593
Rohan24222121 <136613540+Rohan24222121@users.noreply.github.com>
2023-06-14 20:38:45 +0530

    Add files via upload

---
+++ java code.py
@@ -0,0 +1,6 @@
+a=int(input("please enter the first number"))
+b=int(input("please enter the second number"))
+c = int (input(" Please Enter the third number: "))
+average=(a+b+c)/3
+print("The average of three numbers is ", average)
+
